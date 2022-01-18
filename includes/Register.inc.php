279defacfee575e54e34ad3b67323075e2bfb4df
NaoufalAndichi <00313992@rijnijssel.nl>
2022-01-18 15:03:55 +0100

    commit

--- includes/Register.inc.php
+++ includes/Register.inc.php
@@ -1,6 +1,6 @@
 <div class="loginform">
     <h1>Register</h1>
-    <form action="#" method="post">
+    <form action="../php/registreren.php" method="post">
         <input type="text" name="voornaam" placeholder="voornaam">
         <input type="text" name="tussenvoegsel" placeholder="tussenvoegsel">
         <input type="text" name="achternaam" placeholder="achternaam">
@@ -8,9 +8,8 @@
         <input type="text" name="straat" placeholder="straat">
         <input type="text" name="huisnummer" placeholder="huisnummer">
         <input type="text" name="postcode" placeholder="postcode">
-        <input type="text" name="email" placeholder="email">
-        <input type="text" name="wachtwoord" placeholder="wachtwoord">
-        <input type="text" name="geboortedatum" placeholder="geboortedatum">
+        <input type="text" name="username" placeholder="username">
+        <input type="text" name="password" placeholder="password">
         <button type="submit">Register</button>
     </form>
 </div>
--- php/registreren.php
+++ php/registreren.php
@@ -9,10 +9,9 @@ $huisnummer = $_POST['huisnummer'];
 $postcode = $_POST['postcode'];
 $username = $_POST['username'];
 $password = $_POST['password'];
-$geboortedatum = $_POST['geboortedatum'];
 
 
-$stmt = $conn->prepare("INSERT INTO users (voornaam, achternaam,woonplaats,straat,huisnummer,postcode,username,password,geboortedatum) values(:voornaam, :achternaam, :woonplaats, :straat, :huisnummer,:postcode,:username,:password,:geboortedatum)");
+$stmt = $conn->prepare("INSERT INTO users (voornaam, achternaam,woonplaats,straat,huisnummer,postcode,username,password) values(:voornaam, :achternaam, :woonplaats, :straat, :huisnummer,:postcode,:username,:password)");
 
 $stmt->bindParam(':voornaam' , $voornaam);
 $stmt->bindParam(':achternaam' , $achternaam);
@@ -22,7 +21,6 @@ $stmt->bindParam(':huisnummer' , $huisnummer);
 $stmt->bindParam(':postcode' , $postcode);
 $stmt->bindParam(':username' , $username);
 $stmt->bindParam(':password' , $password);
-$stmt->bindParam(':geboortedatum' , $geboortedatum);
 
 
 $stmt->execute();
