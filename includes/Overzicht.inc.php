9090dbbe66235d7251fdee3be119d2daf86708b4
Naoufal Andichi <00313992@rijnijssel.nl>
2022-01-20 21:41:26 +0100

    wat css fixes

--- includes/Overzicht.inc.php
+++ includes/Overzicht.inc.php
@@ -1,5 +1,5 @@
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<div class="w3-row-padding w3-padding-16 w3-center">
+<div class="container">
     <div class="w3-quarter">
         <img src="https://kbimages1-a.akamaihd.net/1a3242ad-3242-42e2-b5eb-41021cf47c58/353/569/90/False/the-fellowship-of-the-ring-the-lord-of-the-rings-book-1-1.jpg" alt="Sandwich" style="width:10%">
         <h3>Lord Of The Rings</h3>
