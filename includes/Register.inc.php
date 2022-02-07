c8863bb1344ba072c2ab7cb2865c070d240e19a6
NaoufalAndichi <00313992@rijnijssel.nl>
2022-02-07 14:04:43 +0100

    fixed +1 issue

--- includes/Register.inc.php
+++ includes/Register.inc.php
@@ -1,15 +1,15 @@
 <div class="loginform">
     <h1>Register</h1>
     <form action="../php/register.php" method="post">
-        <input type="text" name="voornaam" placeholder="voornaam">
-        <input type="text" name="tussenvoegsel" placeholder="tussenvoegsel">
-        <input type="text" name="achternaam" placeholder="achternaam">
-        <input type="text" name="woonplaats" placeholder="woonplaats">
-        <input type="text" name="straat" placeholder="straat">
-        <input type="text" name="huisnummer" placeholder="huisnummer">
-        <input type="text" name="postcode" placeholder="postcode">
-        <input type="text" name="username" placeholder="username">
-        <input type="password" name="password" placeholder="password">
+        <input type="text" name="voornaam" placeholder="voornaam" required>
+        <input type="text" name="tussenvoegsel" placeholder="tussenvoegsel" required>
+        <input type="text" name="achternaam" placeholder="achternaam" required>
+        <input type="text" name="woonplaats" placeholder="woonplaats" required>
+        <input type="text" name="straat" placeholder="straat" required>
+        <input type="text" name="huisnummer" placeholder="huisnummer" required>
+        <input type="text" name="postcode" placeholder="postcode" required>
+        <input type="text" name="username" placeholder="username" required>
+        <input type="password" name="password" placeholder="password" required>
         <button type="submit">Register</button>
     </form>
 </div>
